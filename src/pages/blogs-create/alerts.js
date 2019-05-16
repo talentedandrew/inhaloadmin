0f931fb710f669978ba2c993fd9c9dda198d27e8
SenelithPerera <senelith2018@gmail.com>
2019-05-16 11:30:41 +0530

    added sweet alert for image upload

--- src/pages/blogs-create/alerts.js
+++ src/pages/blogs-create/alerts.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import SweetAlert from 'sweetalert2-react';
+// import React from 'react';
+// import SweetAlert from 'sweetalert2-react';
+// import swal from 'sweetalert';
+
+// class SuccessAlert extends React.Component {
+
+//     state = {
+//         show: false
+//     }
 
 
-class SuccessAlert extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 {/* <button onClick={() => this.setState({ isShow: true })}>Alert</button> */}
+//                 <SweetAlert
+//                     show={() => this.setState({ show: true })}
+//                     title="Demo"
+//                     text="SweetAlert in React"
+//                     onConfirm={() => this.setState({ show: false })}
+//                 />
+//             </div>
+//         );
+//     }
 
-    state = {
-        show: false
-    }
 
-
-    render() {
-        return (
-            <div>
-                {/* <button onClick={() => this.setState({ isShow: true })}>Alert</button> */}
-                <SweetAlert
-                    show={() => this.setState({ show: true })}
-                    title="Demo"
-                    text="SweetAlert in React"
-                    onConfirm={() => this.setState({ show: false })}
-                />
-            </div>
-        );
-    }
-}
-
-export default SuccessAlert;
+// export default SuccessAlert;
